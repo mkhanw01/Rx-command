b5ff5ea067749873ee6723f9ab512df24836015a
waseem.khan <mohammad.waseem@coviam.com>
2017-12-28 17:53:33 +0530

    first commit

---
+++ command-web/src/main/java/CommandApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by khan on 12/28/17.
+ */
+@SpringBootApplication()
+public class CommandApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CommandApplication.class, args);
+  }
+}
