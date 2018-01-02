a5da6b28d7c1f157187fef399f0dad97f4db1f82
waseem.khan <mohammad.waseem@coviam.com>
2018-01-02 14:11:07 +0530

    master: added api path and PingController

---
+++ command-model/src/main/java/com/waseem/command/model/controller/ApiPath.java
@@ -0,0 +1,14 @@
+package com.waseem.command.model.controller;
+
+/**
+ * Created by khan on 1/2/18.
+ */
+public interface ApiPath {
+
+   String CONTEXT_PATH = "/x-command";
+
+   String PING_BASE_PATH = "/pingController";
+   String HOME =  "/";
+   String PING = "/ping";
+
+}
---
+++ command-web/src/main/java/com/waseem/command/controller/PingController.java
@@ -0,0 +1,51 @@
+package com.waseem.command.controller;
+
+import com.waseem.command.model.controller.ApiPath;
+import com.waseem.command.model.controller.Response;
+import com.waseem.command.model.service.PingRequest;
+import com.waseem.command.service.ServiceExecutor;
+import com.waseem.command.service.command.PingCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+/**
+ * Created by khan on 12/30/17.
+ */
+@RestController
+@RequestMapping(value = ApiPath.PING_BASE_PATH)
+public class PingController {
+
+  @Value("${server.context-path}")
+  private String application;
+
+  @Autowired
+  private ServiceExecutor serviceExecutor;
+
+
+  @GetMapping(value = ApiPath.HOME, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Mono<Response<String>> home() {
+    PingRequest pingRequest = PingRequest.builder().build();
+
+    return returnExecutor(pingRequest);
+  }
+
+  private Mono<Response<String>> returnExecutor(PingRequest pingRequest) {
+
+    return this.serviceExecutor.execute(PingCommand.class, pingRequest)
+        .map(Response::ok)
+        .subscribeOn(Schedulers.elastic());
+  }
+
+  @GetMapping(value = ApiPath.PING, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Mono<Response<String>> ping(){
+    PingRequest request = PingRequest.builder().build();
+
+    return returnExecutor(request);
+  }
+}
